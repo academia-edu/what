b01f21cec567d25405c56dcce3e63720438e434b
Ryan Fitzgerald <rwfitzge@gmail.com>
2011-05-18 14:01:23 -0700

    add module for monitoring other what instances

--- lib/what/modules/what.rb
+++ lib/what/modules/what.rb
@@ -1,33 +1,26 @@
-# FIXME: currently assumes the other servers are set to JSON
-class What::Modules::What < What::Modules::Base
-  def initialize
-    super
-    @config.each do |name, host|
-      @config[name] = "http://#{host}:9428"
+# This module assumes the other servers are using the JSON formatter.
+module What
+  class Modules::What < Modules::Base
+    def initialize
+      super
+      @config.each do |name, host|
+        @config[name] = "http://#{host}:9428"
+      end
+      @whats = {}
     end
-    @whats = {}
-  end
 
-  def check!
-    @config.map do |name, uri|
-      curl = Curl::Easy.new(uri)
-      curl.on_complete do |easy|
-        @whats[name] = JSON.parse(easy.body_str)
+    def check!
+      @config.map do |name, uri|
+        Helpers.curl(uri) { |body| @whats[name] = JSON.parse(body) rescue nil }
       end
     end
-  end
 
-  def health
-    all_ok = true
-    @whats.each do |name, results|
-      if results[:health] != :ok
-        all_ok = false
-      end
+    def health
+      Helpers.overall_health(@whats.map { |_, attrs| attrs[:health] })
     end
-    all_ok ? :ok : :alert
-  end
 
-  def details
-    @whats
+    def details
+      @whats
+    end
   end
 end
