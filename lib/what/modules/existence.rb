d9e7995bb6ce440fd64c745482001efb776164f4
Ryan Fitzgerald <rwfitzge@gmail.com>
2011-05-18 15:38:46 -0700

    add file existence module, bump to v0.0.11

---
+++ lib/what/modules/existence.rb
@@ -0,0 +1,30 @@
+module What
+  class Modules::Existence < Modules::Base
+    DEFAULTS = {
+      'paths' => []
+    }
+
+    def initialize
+      super
+      @paths = {}
+    end
+
+    def check!
+      @config['paths'].each do |path|
+        if Dir[path].count == 0
+          @paths[path] = false
+        else
+          @paths[path] = true
+        end
+      end
+    end
+
+    def health
+      Helpers.overall_health(@paths.map { |k, v| v ? 'ok' : 'alert' })
+    end
+
+    def details
+      @paths
+    end
+  end
+end
