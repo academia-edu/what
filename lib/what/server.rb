29d4517a0fadd090869a965fdf327bcf2ca7d3e8
Ryan Fitzgerald <rwfitzge@gmail.com>
2011-05-20 14:06:33 -0700

    add optional secret token, bump to v0.0.16

--- lib/what/server.rb
+++ lib/what/server.rb
@@ -6,12 +6,16 @@ module What
       Monitor.go!
     end
 
-    def call(_)
-      [
-        Status['health'] != 'alert' ? 200 : 503,
-        {'Content-Type' => Formatter.mime},
-        Formatter.format(Status.all)
-      ]
+    def call(env)
+      if Config['secret_token'] && Config['secret_token'] != env['QUERY_STRING']
+        [403, {'Content-Type' => 'text/plain'}, "403 Forbidden\n"]
+      else
+        [
+          Status['health'] != 'alert' ? 200 : 503,
+          {'Content-Type' => Formatter.mime},
+          Formatter.format(Status.all)
+        ]
+      end
     end
   end
 end
