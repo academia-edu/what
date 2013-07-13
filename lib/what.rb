e1a8f42bb7385283fb7175466973717fe4502386
Ryan Fitzgerald <rwfitzge@gmail.com>
2013-07-12 18:40:20 -0700

    Simplify config loading

--- lib/what.rb
+++ lib/what.rb
@@ -1,8 +1,6 @@
 module What
 end
 
-require 'pry'
-
 require 'json'
 require 'yaml'
 require 'webrick'
--- lib/what/modules.rb
+++ lib/what/modules.rb
@@ -1,58 +1,29 @@
-require 'what/config'
+require "what/config"
 
 module What
   module Modules
-
-    def self.default_modules_full_path
-      File.join(File.dirname(__FILE__), 'modules')
-    end
-
-    def self.config_module_full_paths
-      (Config['module_paths'] || []).map do |p|
-        if p.match(%r(^/))
-          p
-        else
-          File.join(Config['base'], p)
-        end
-      end
-    end
-
-    # load all modules defined in what/modules, in addition to any paths
-    # specified in the config file.
+    # Load all modules from any paths specified in the config file.
     def self.load_all
-      require 'what/modules/base'
-
-      require_dir(self.default_modules_full_path)
-
-      self.config_module_full_paths.each do |path|
+      config_module_full_paths.each do |path|
         require_dir(path)
       end
     end
 
-    def self.load_module(module_name)
-      require 'what/modules/base'
+    def self.config_module_full_paths
+      paths = Config["module_paths"] || []
 
-      loaded = false
-
-      short_module_name = module_name.sub(/\.rb$/, '')
-
-      ([self.default_modules_full_path] + self.config_module_full_paths).each do |path|
-        candidate = File.join(path, short_module_name)
-        if File.exists?("#{candidate}.rb")
-          loaded = require candidate
-        end
+      paths.map do |path|
+        path.include?("/") ? path : File.join(Config["base"], path)
       end
-
-      loaded
     end
-
-    private
 
     def self.require_dir(path)
       Dir[File.join(path, '*.rb')].each do |fn|
         require fn
       end
     end
-
   end
 end
+
+require "what/modules/base"
+What::Modules.require_dir File.expand_path("../modules", __FILE__)
